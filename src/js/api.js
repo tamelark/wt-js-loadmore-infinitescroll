35bdf629897239d1841acff86a971d5596ef68c8
Jemin Kipkin <tamelarkdagora@gmail.com>
2023-12-22 00:07:11 +0200

    insult me in a line , compliment me on the next

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '41256071-ed92c4fb3a3837c10316edc61';
+const API_KEY = '41395119-a3e7aee08cabbf76e0f4c62d0';
 const BASE_URL = 'https://pixabay.com/api/';
 const DEFAULT_PER_PAGE = 40;
 
